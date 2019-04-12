a802caa40d3a96133fbe53adba403406359f5b23
aophelie <oamolomakama.pv@gmail.com>
2019-04-12 16:57:44 +0200

    Deuxième version avec la création d'un dataframe à partir d'un fichier donné et de SparkSession (Spark Sql)

--- build.sbt
+++ build.sbt
@@ -6,6 +6,8 @@ scalaVersion := "2.11.8"
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.2"
 
+libraryDependencies += "org.apache.spark" % "spark-sql_2.11" % "2.0.0"
+
 libraryDependencies += "org.apache.jena" % "jena-core" % "3.10.0"
 
 libraryDependencies += "org.apache.jena" % "jena-arq" % "3.10.0"
--- src/main/scala/JenaQuery.scala
+++ src/main/scala/JenaQuery.scala
@@ -1,14 +1,11 @@
 
-import org.apache.jena.query.{Query, QueryExecutionFactory, QueryFactory}
-import org.apache.jena.rdf.model.ModelFactory
-import org.apache.jena.sparql.algebra.OpAsQuery.Converter
-import org.apache.jena.sparql.algebra.op._
-import org.apache.jena.sparql.algebra.walker.WalkerVisitor
+import org.apache.jena.query.QueryFactory
 import org.apache.jena.sparql.algebra._
-import org.apache.jena.sparql.lang.SPARQLParser
-import org.apache.spark
 import org.apache.jena.sparql.algebra.walker.Walker
 import visitor._
+import org.apache.spark.sql.SparkSession
+
+
 class JenaQuery extends App {
 
 
@@ -34,13 +31,10 @@ class JenaQuery extends App {
       "Filter (?o > 32)" +
       "} "
 
-    //val model = ModelFactory.createDefaultModel()
-
-    //val qr = QueryFactory.create(myQuery)
-    //val query = QueryFactory.parse(new Query(), myQuery, prefix, )
-
+    //Création de la requête à partir de la variable String myQuery
     val query = QueryFactory.create(myQuery)
-    // Transformation en algebra
+
+    //Transformation de la requête en algèbre relationnel
     val op = Algebra.compile(query)
    // println("op : " + op)
 
@@ -49,21 +43,43 @@ class JenaQuery extends App {
 
     println("op1 : " + op1)
 
-    //walkerVisitor
 
-    // Parcours de ce dernier
-    //op.visit(new Visitor)
+
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////// TEST VISITOR POUR PARCOURIR DE L'ALGEBRE /////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Parcours de l'algèbre obtenu en utilisant notre visitor
 
     Walker.walk(op1,new Visitor)
 
 
-    //println("op1 : " + op1)
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////// CRETION DATAFRAME EN UTILISANT SPARKSESSION //////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////
 
-    /*val rs = query.execSelect();
+    val spark = SparkSession
+      .builder()
+      .appName("Spark SQL basic example")
+      .config("spark.some.config.option", "some-value")
+      .getOrCreate()
 
-    while (rs.hasNext()) {
-      print(rs.next())
-    }*/
+    //variante création SparkSession
+    //val spark = SparkSession.builder().appName("testings").master("local").config("configuration key", "configuration value").getOrCreate
+
+    // For implicit conversions like converting RDDs to DataFrames (toDF,...)
+    import spark.implicits._
+
+    val sqlContext = spark.sqlContext
+
+    val mydataframe = spark.read.text("D:\\M2-IFLogiciels\\IA\\LUBM encodés-20190321\\LUBMInstances\\part-00000.txt")
+
+    /*val dataframe = spark.read.format("csv")
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .load("train_users_2.csv")*/
+
+
   }
 
 
